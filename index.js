4126b461fa782997f389656e7d24ebf67c2aa20d
colekimchi <colekimchi@gmail.com>
2021-07-18 22:15:20 -0700

    Added preliminary footer element

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 function loadNav() {
     $("#nav-bar-container").load("components/navigation.html");
 }
+function loadFooter() {
+    $("#footer-container").load("components/footer.html");
+}
 
 $(function() {
     loadNav();
+    loadFooter();
 });
