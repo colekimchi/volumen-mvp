557b8a56d4062cc21a1f42e62503e79e54b5abd2
Cole Kim <colekimchi@gmail.com>
2021-08-17 15:58:23 -0700

    Added Popular Items Carousel

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ function loadNav() {
 function loadFooter() {
     $("#footer-container").load("components/footer.html");
 }
+function loadCarousel(){
+    $("#carousel-container").load("components/article_carousel.html");
+}
 
 $(function() {
     loadNav();
     loadFooter();
+    loadCarousel();
 });
